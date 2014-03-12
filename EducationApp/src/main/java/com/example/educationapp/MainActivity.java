4b3f861130826705798acd5f736d1d94a1f34ef7
kevin <kevinfg05@gmail.com>
2014-03-12 22:07:49 +0800

    first commit

---
+++ EducationApp/src/main/java/com/example/educationapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.educationapp;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import com.actionbarsherlock.app.SherlockActivity;
+import android.os.Build;
+
+public class MainActivity extends SherlockActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
