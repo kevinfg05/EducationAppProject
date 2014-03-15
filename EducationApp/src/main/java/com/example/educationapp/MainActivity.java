7845373672cf7e37a7842e02ca9216d7a915ecee
kevin <kevinfg05@gmail.com>
2014-03-15 14:15:05 +0800

    user info activity

--- EducationApp/src/main/java/com/example/educationapp/MainActivity.java
+++ EducationApp/src/main/java/com/example/educationapp/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends SherlockActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSherlock().getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     /**
---
+++ EducationApp/src/main/java/com/example/educationapp/UserInfoActivity.java
@@ -0,0 +1,15 @@
+package com.example.educationapp;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+/**
+ * Created by kevin on 14-3-13.
+ */
+public class UserInfoActivity extends SherlockActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
